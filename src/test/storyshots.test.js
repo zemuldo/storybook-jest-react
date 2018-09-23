9a1e88458ee351e9671766fec1b2652ed9eb4d2c
Danstan Onyango ~ Zemuldo <danstan.otieno@gmail.com>
2018-09-23 15:06:31 +0300

    adds jest snapshots

---
+++ src/test/storyshots.test.js
@@ -0,0 +1,10 @@
+import initStoryshots from '@storybook/addon-storyshots';
+
+initStoryshots({ /* configuration options */ });
+
+const DATE_TO_USE = new Date('2016');
+const _Date = Date;
+global.Date = jest.fn(() => DATE_TO_USE);
+global.Date.UTC = _Date.UTC;
+global.Date.parse = _Date.parse;
+global.Date.now = _Date.now;
